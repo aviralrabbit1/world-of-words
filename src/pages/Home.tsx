377ed0c17f26331c95c3bf03e41244c25a21f0ec
aviralrabbit1 <54033735+aviralrabbit1@users.noreply.github.com>
2023-09-17 02:37:10 +0530

    Added Home and NotFound Page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div>
+      World of Words is a playful project, utilising words and their magic!
+    </div>
+  );
+};
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export const NotFound = () => {
+  return (
+    <div>
+      <p>Hmmmmm.......</p>
+      <p>I wonder where this page went!</p>
+    </div>
+  );
+};
