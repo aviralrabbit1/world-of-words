06d13e636a12f6eeddc211c65d4ac21f8e8c212a
aviralrabbit1 <54033735+aviralrabbit1@users.noreply.github.com>
2023-09-17 03:56:07 +0530

    minor CSS changes

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import './Router.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 
 import { Home } from './pages/Home';
 import Antonym from './pages/Antonym';
